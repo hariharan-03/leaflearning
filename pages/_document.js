3dfa77ecd35eea6c43baf7f135541168aaf4f927
hariharan-03 <60616827+hariharan-03@users.noreply.github.com>
2023-01-05 18:13:56 +0530

    og added

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
       <link rel="icon" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <meta property="og:title" content="Dumbcoder's Template" />
+      <meta property="og:description" content="Leaf Learning template." />
+      <meta property="og:image" content="images/og-image.png" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
